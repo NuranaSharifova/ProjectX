5c366f07ffecf686688fd3f0d4dfb81a451c2595
Sari_nu95@itstep.edu.az <Sari_nu95@itstep.edu.az>
2021-04-06 21:51:53 +0400

    Last updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include<iostream>
+#include <vector>
 using namespace std;
-void main() {
-	cout << "Helo" << endl;
-	cout << "Hello World" << endl;
+
+
+
+class Car {
+	string cname;
+	int year;
+	int volume;
+	int price;
+public:
+	Car()
+	{
+		string name = "";
+		year = 0;
+		volume = 0;
+		price = 0;
+	}
+	Car(string _cname,int _year,int _volume,int _price)
+	{
+		this->cname = _cname;
+		this->year = _year;
+		this->volume = _volume;
+		this->price = _price;
+	}
+
+
+
+};
+void main(){
+
+	vector<Car>v;
+	Car c{ "Toyota",1975,300,50000 };
+	v.push_back(c);
+
+
 
 }
